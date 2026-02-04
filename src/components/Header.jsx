12117bafed82d30c19c40d9fad3e0d489824149a
Olawale <eyakubu442@gmail.com>
2026-02-04 15:36:41 +0100

    logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,21 @@ import { Navbar, Container, Nav } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { FaHome, FaRegUser, FaRegSun } from "react-icons/fa";
 import { MdOutlineContactMail } from "react-icons/md";
+import logo from '../assets/logo.jpg';
 
 const Header = () => {
     return (
         <Navbar bg="dark" variant="dark" expand="lg">
             <Container>
-                <Navbar.Brand as={Link} to='/'>Olawale</Navbar.Brand>
+                <Navbar.Brand as={Link} to='/'>
+                    <img
+                        src={logo}
+                        alt='logo'
+                        width='40'
+                        height='40'
+                        className='d-inline-block align-top rounded-circle'
+                    />
+                </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ms-auto">
